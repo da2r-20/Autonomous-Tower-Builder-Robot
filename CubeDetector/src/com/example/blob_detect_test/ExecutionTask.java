001f66ebcaf2ffd5025c5ebc91afed5c19d29e5b
Pavel Rubinson <pavelr@gmail.com>
2014-09-11 00:19:34 +0300

    Add new and improved movement algorithm that replaces everythinggggggggggg

--- CubeDetector/src/com/example/blob_detect_test/ExecutionTask.java
+++ CubeDetector/src/com/example/blob_detect_test/ExecutionTask.java
@@ -12,15 +12,40 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 	public AsyncResponse delegate = null;
 	private MovmentSystem _movmentSystem;
 	private boolean isMoving = false;
+	
+	//The array of colors that we are searching for, in the order of their appearance in the tower.
+	//First member is the base cube.
 	private Color[] colorArr;
+	
+	//The index (in colorArr) of the current color we are looking for
 	private int currColor;
+	
+	//Horizontal center threshold (from -center to +center)
 	private int center = 30;
+	
+	//Stopping "distance" from the cube (roughly corresponds to cm, but not really)
 	private int distance = 15;
+	
+	//Robot speed values
+	private double moveSpeed = 0.4;
+	private double turnSpeed = 0.7;
+	
+	//True if we are going to the base cube. False otherwise. 
 	private boolean gotoBase;
+	
+	//Values to pass to the robotMove method
 	private final static int MOVE = 1;
 	private final static int RIGHT = 2;
 	private final static int LEFT = 3;
 	private final static int STOP = 0;
+	
+	private final static int SEARCH = 1;
+	private final static int CENTER_LEFT = 2;
+	private final static int CENTER_RIGHT = 3;
+	private final static int GOTO = 4;
+	private final static int DONE = 5;
+	
+	private int state = SEARCH;
 	
 	
 	ExecutionTask(AsyncResponse delegate, MovmentSystem movmentSystem, Color[] colorArr){
@@ -45,10 +70,11 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		double horizLoc, distance;
+		//double horizLoc, distance;
 		Log.i("", "Starting main loop");
 		
-		while (this.colorArr.length - this.currColor > 0){
+		this.currColor = 1;
+		while (this.currColor < this.colorArr.length){
 			if (isCancelled()){
 				Log.i("", "Task stopped");
 				try {
@@ -56,18 +82,41 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 				} catch (ConnectionLostException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 				break;
 			}
 			if (this.gotoBase){
 				CubeInfo.getInstance().setColor(this.colorArr[0]);
+				Log.i("Color change", "Color index is now: " + 0);
 				this.gotoBase = false;
+				currColor++;
 			} else {
 				CubeInfo.getInstance().setColor(this.colorArr[currColor]);
+				Log.i("Color change", "Color index is now: " + String.valueOf(currColor));
 				this.gotoBase = true;
-				currColor++;
+			}
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 			
+			try {
+				this.magicalAlgorithm();
+			} catch (ConnectionLostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			
+			/*
 			try {
 				this.searchForCube();
 			} catch (ConnectionLostException e) {
@@ -88,6 +137,7 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			*/
 		}
 		
 		//while(true){
@@ -165,7 +215,6 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 				break;
 			}
 			//Log.i("", "Found? " + CubeInfo.getInstance().getFound());
-			//TODO robot turn right/left
 			this.robotMove(RIGHT);
 			Thread.sleep(100);
 			this.robotMove(STOP);
@@ -173,7 +222,7 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 			
 		}
 		this.robotMove(STOP);
-		
+		 horizLoc = CubeInfo.getInstance().getHorizontalLocation();
 		while (horizLoc < -this.center || horizLoc > this.center){
 			Log.i("", "Horizontal location: " + String.valueOf(horizLoc));
 			if (isCancelled()){
@@ -195,7 +244,6 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 		this.robotMove(STOP);
 		//Log.i("", "STOP!");
 		//Log.i("", "Horizontal location at stop: " + String.valueOf(horizLoc));
-		
 	}
 	
 	protected void goToCube() throws ConnectionLostException, InterruptedException{
@@ -203,7 +251,7 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 		Log.i("", "Going to cube");
 		double horizLoc = CubeInfo.getInstance().getHorizontalLocation();
 		
-		while (CubeInfo.getInstance().getDistance() > this.distance){
+		while (CubeInfo.getInstance().getDistance() > this.distance || !CubeInfo.getInstance().getFound()){
 			if (isCancelled()){
 				this.robotMove(STOP);
 				break;
@@ -211,13 +259,13 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 			//Log.i("", "goToCube distance: " +  String.valueOf(CubeInfo.getInstance().getDistance()));
 			horizLoc = CubeInfo.getInstance().getHorizontalLocation();
 			//Log.i("", "goToCube horizontal location" +  String.valueOf(horizLoc));
-			if (horizLoc > -this.center && horizLoc < this.center){
+			if (CubeInfo.getInstance().getFound() && horizLoc > -this.center && horizLoc < this.center){
 				//Log.i("", "MOVE!");
 				//this._movmentSystem.moveForwardCont();
 				this.robotMove(MOVE);
-				//Thread.sleep(100);
+				//Thread.sleep(20);
 				//this.robotMove(STOP);
-				//Thread.sleep(100);
+				//Thread.sleep(20);
 				//Log.i("", "Going to cube, horizontal location: " +  String.valueOf(horizLoc));
 			}
 			else {
@@ -226,38 +274,89 @@ public class ExecutionTask extends  AsyncTask<URL, Integer, Long>{
 				this.searchForCube();
 			}
 		}	
-		//Log.i("", "STOP!");
-		
+		//Log.i("", "STOP!");	
 		this.robotMove(STOP);
-		Thread.sleep(1000);
+		Thread.sleep(3000);
 	}
 	
-	private void robotMove(int movement) throws ConnectionLostException{
+	private void robotMove(int movement) throws ConnectionLostException, InterruptedException{
 		if (this.isMoving){
 			if (movement == STOP){
 				this.isMoving = false;	
 				Log.i("", "Stop command issued");
 				this._movmentSystem.stop();			
 			}	
-		} else if (movement > STOP) {
+		} else if (movement != STOP) {
 			this.isMoving = true;
-			_movmentSystem.setRoverSpeed((float)0.7);
+			_movmentSystem.setRoverSpeed((float)turnSpeed);
 			switch (movement){
 			case(MOVE):
 				Log.i("", "Issued move command");
-				_movmentSystem.setRoverSpeed((float)0.4);
+				_movmentSystem.setRoverSpeed((float)moveSpeed);
 				this._movmentSystem.moveForwardCont();
 				break;
-			case(RIGHT):
+			case(RIGHT):				
 				Log.i("", "Issued turn right command");	
 				this._movmentSystem.turnRight();
+				Thread.sleep(100);
+				this.robotMove(STOP);
+				Thread.sleep(100);
 				break;
 			case(LEFT):
 				Log.i("", "Issued turn left command");	
 				this._movmentSystem.turnLeft();
+				Thread.sleep(100);
+				this.robotMove(STOP);
+				Thread.sleep(100);
 				break;
 			}
 		}	
+	}
+
+	
+	private void magicalAlgorithm() throws ConnectionLostException, InterruptedException{
+		this.state = SEARCH;
+		while (this.state != DONE){
+			this.updateState();
+			switch (this.state){
+				case(SEARCH):
+					this.robotMove(RIGHT);
+					break;
+				case(CENTER_LEFT):
+					this.robotMove(LEFT);
+					break;
+				case(CENTER_RIGHT):
+					this.robotMove(RIGHT);
+					break;
+				case(GOTO):
+					this.robotMove(MOVE);
+					break;
+			}
+		}
+	}
+	
+	private void updateState() throws ConnectionLostException, InterruptedException{
+		double currHorizLoc = CubeInfo.getInstance().getHorizontalLocation();
+		double currDist = CubeInfo.getInstance().getDistance();
+		if (!CubeInfo.getInstance().getFound()){
+			this.setState(SEARCH);
+		} else if (currHorizLoc < -this.center){
+			this.setState(CENTER_LEFT);
+		} else if (currHorizLoc > this.center){
+			this.setState(CENTER_RIGHT);
+		} else if (currDist > this.distance){
+			this.setState(GOTO);
+		} else {
+			this.setState(DONE);
+		}
+	}
+	
+	
+	private void setState(int newState) throws ConnectionLostException, InterruptedException{
+		if (this.state != newState){
+			robotMove(STOP);
+			this.state = newState;
+		}
 	}
 
 	protected void onProgressUpdate(Integer... progress) {
