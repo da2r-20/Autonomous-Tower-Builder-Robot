cb5d8af2e2ba7f5c44ff63334562680fa2523369
Pavel Rubinson <pavelr@gmail.com>
2014-10-13 11:23:19 +0300

    Add moving average helper class
    This class allows to calculate the moving average of n numbers.
    It is currently used in the project to smooth out the distance readings
    given by the sensor.

---
+++ CubeDetector/src/com/example/blob_detect_test/MovingAvg.java
@@ -0,0 +1,33 @@
+package com.example.blob_detect_test;
+
+
+public class MovingAvg {
+	private int index = 0;
+	private int size;
+	private int count = 0;
+	private double total = 0;
+	private double data[];
+	
+	public MovingAvg(int size){
+		this.size = size;
+		data = new double[size];
+		for (int i=0; i<size; i++){
+			data[i] = 0;
+		}
+	}
+	
+	public void update(double newItem){
+		if (this.count < this.size){
+			this.count ++;
+		}
+		this.total -= this.data[index];
+		this.total += newItem; 
+		this.data[index] = newItem;
+		this.index = (this.index + 1)%this.size;
+	}
+	
+	public double getAvg(){
+		return this.total/this.count;
+	}
+
+}
